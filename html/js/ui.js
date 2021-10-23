c3ce778e91cf6f3daf8e1df780ad4aaea9b4be42
Plush777 <sky11916@naver.com>
2021-10-23 15:54:44 +0900

    다크모드클래스수정

--- html/js/ui.js
+++ html/js/ui.js
@@ -3,44 +3,14 @@ $(document).ready(function(){
     $(".quick-btn").click(function() {
         $('html').animate({scrollTop : 0}, 600);
     });
-});
 
-// /* 팝업 close 닫기버튼  */
-// $(document).ready(function () {
-// 	$('.btn-close').click(function(){
-// 		$('.layer-popup').hide();
-// 	});
-
-// 	$('.btn-close').click(function(){
-// 		$('.popup-mask').hide();
-// 	});
-// });
-
-
-// //퀵메뉴1
-// $(document).ready(function() {
-//     var floatPosition = parseInt($(".quick-btn").css('top'));
-// 	$(window).scroll(function() {
-// 		var scrollTop = $(window).scrollTop();
-// 		var newPosition = scrollTop + floatPosition + "px";
-// 		$(".quick-btn").stop().animate({
-// 			"top" : newPosition
-// 		}, 500);
-// 	}).scroll();
-// });
-
-
-//탭 리스트
-$(document).ready(function(){
 	$('.tabList li').on({
 		"click":function(){
 			$(this).addClass('active').siblings('li').removeClass('active');
 			$(this).closest('.tabNav').siblings('.tabCont').eq($(this).index()).addClass('active').siblings('.tabCont').removeClass('active');
 		}
 	})
-});
-/* 아코디언 메뉴 토글 */
-$(document).ready(function(){
+
 	$('.accordion .dep1>li>.head').on({
 		'click': function click(e) {
 		  e.preventDefault();
@@ -48,15 +18,16 @@ $(document).ready(function(){
 		  $(this).closest('li').children('.dep2').stop().slideToggle(400).closest('li').siblings('li').children('.dep2').slideUp(400);
 		}
 	});	
-});
 
 
-/* PC 다크모드 */
-$(document).ready(function(){
+	/* PC 다크모드 */
 	const storedTheme = localStorage.getItem("darkTheme");
+	const storedThemeLight = localStorage.getItem("lightTheme");
 
-	document.getElementById("darklight").addEventListener("click", () => {
-        const html = document.documentElement;
+	/* dark class 일때 */
+	var darkEvent = document.getElementsByClassName('darkToggle');
+	function localSetDark() {
+		const html = document.documentElement;
         if (html.classList.contains("dark")) {
             html.classList.remove("dark");
             localStorage.setItem("darkTheme", "false");
@@ -64,8 +35,12 @@ $(document).ready(function(){
             html.classList.add("dark");
             localStorage.setItem("darkTheme", "true");
         }
-    });
-    if (storedTheme !== null) {
+	}
+	for (var i = 0; i < darkEvent.length; i++) {
+		darkEvent[i].addEventListener('click', localSetDark);
+	}
+
+	if (storedTheme !== null) {
         if (storedTheme === "true") {
             document.documentElement.classList.add("dark");
         }
@@ -73,27 +48,31 @@ $(document).ready(function(){
         document.documentElement.classList.add("dark");
     }
 
-	document.getElementById("mobile-darklight").addEventListener("click", () => {
-        const html = document.documentElement;
-        if (html.classList.contains("dark")) {
-            html.classList.remove("dark");
-            localStorage.setItem("darkTheme", "false");
+
+	/* light 클래스일때 */
+	var lightEvent = document.getElementsByClassName('darkToggle');
+	function localSetLight() {
+		const html = document.documentElement;
+        if (html.classList.contains("light")) {
+            html.classList.remove("light");
+            localStorage.setItem("lightTheme", "false");
         } else {
-            html.classList.add("dark");
-            localStorage.setItem("darkTheme", "true");
+            html.classList.add("light");
+            localStorage.setItem("lightTheme", "true");
         }
-    });
-    if (storedTheme !== null) {
-        if (storedTheme === "true") {
-            document.documentElement.classList.add("dark");
+	}
+	for (var i = 0; i < lightEvent.length; i++) {
+		lightEvent[i].addEventListener('click', localSetLight);
+	}
+
+	if (storedThemeLight !== null) {
+        if (storedThemeLight === "true") {
+            document.documentElement.classList.add("light");
         }
-    } else if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
-        document.documentElement.classList.add("dark");
+    } else if (window.matchMedia("(prefers-color-scheme: light)").matches) {
+        document.documentElement.classList.add("light");
     }
-});
 
-
-$(document).ready(function(){
 	/*모바일 메뉴 클릭 시 모바일 사이드 메뉴와 dimmed가 보여짐. */
 	$('.mobile-nav').on({
 		"click":function(){
@@ -119,4 +98,16 @@ $(document).ready(function(){
 		$(this).remove();
 		$('.mobile-nav-right').hide();
 	});
+
+	// //퀵메뉴1
+	// $(document).ready(function() {
+	//     var floatPosition = parseInt($(".quick-btn").css('top'));
+	// 	$(window).scroll(function() {
+	// 		var scrollTop = $(window).scrollTop();
+	// 		var newPosition = scrollTop + floatPosition + "px";
+	// 		$(".quick-btn").stop().animate({
+	// 			"top" : newPosition
+	// 		}, 500);
+	// 	}).scroll();
+	// });
 });
